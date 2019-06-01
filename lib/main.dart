7bba04bf90d49e8f3d116f27456569c72667aa23
DaniyalAbbasi <36009198+abbasidaniyal@users.noreply.github.com>
2019-06-01 16:22:11 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:scoped_model/scoped_model.dart';
 
 import './pages/welcome_page.dart';
 import './scoped_models/main_model.dart';
+import './pages/landing_page.dart';
 
 void main() {
   runApp(Delve());
@@ -19,7 +20,7 @@ class Delve extends StatelessWidget {
           primaryColor: Color.fromRGBO(28, 91, 153, 1),
           accentColor: Color.fromRGBO(255, 250, 245, 1),
         ),
-        home: WelcomePage(),
+        home: LoginPage(),
       ),
     );
   }
